1ca1fe12293b4fd7d2627ef6d0e91589e5208d19
Moonflakes <33527879+Moonflakes@users.noreply.github.com>
2023-06-08 11:40:54 +0200

    add introduction

---
+++ src/components/Introduction.story.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+  // eslint-disable-next-line comma-dangle
+  name: 'TodoItem',
+}
+</script>
+<template>
+  <Story
+    group="top"
+    docs-only
+    icon="carbon:bookmark"
+  />
+</template>
+
+<docs lang="md">
+<h1>Margot THIERY</h1>
+<h3 style="color:rgb(var(--_histoire-color-primary-500));">Développeuse fullstack JS</h3>
+<img src="/profil-1-square.png" style="border-radius: 50%; width: 30%;" alt="profil" title="profil">
+<p>Ancienne neuropsychologue reconvertie dans la programmation. Je cherche un emploi de développeuse JS orienté front avec édition back si nécessaire pour des applications web ou mobile. J’ai une appétence particulière pour l’UI-UX et de l’expérience dans la réalisation de maquettes.</p>
+
+---
+
+<h4>RÉSEAUX</h4>
+<div class="content">
+    <div class="item">
+        <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/linkedin.svg" width="20" height="20" class="color-svg">
+        <a href="https://www.linkedin.com/in/margot-thiery-4bb5a291/">@margot-thiery</a>
+    </div>
+    <div class="item">
+        <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/github.svg" width="20" height="20" class="color-svg">
+        <a href="https://github.com/Moonflakes">@moonflakes</a>
+    </div>
+</div>
+
+---
+
+<h4>ACCESSIBILITÉ</h4>
+<div class="content">
+    <div class="item">
+        <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/solid/house.svg" width="20" height="20" class="color-svg">
+        <p>Clichy</p>
+    </div>
+    <div class="item">
+        <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/solid/location-dot.svg" width="20" height="20" class="color-svg">
+        <p>Île de france</p>
+    </div>
+    <div class="item">
+        <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/solid/laptop.svg" width="20" height="20" class="color-svg">
+        <p>Full remote ou hybride</p>
+    </div>
+</div>
+
+---
+
+<h4>CONTACTS</h4>
+<div class="content">
+    <div class="item">
+        <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/solid/mobile.svg" width="20" height="20" class="color-svg">
+        <p>07 60 75 21 50</p>
+    </div>
+    <div class="item">
+        <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/solid/envelope.svg" width="20" height="20" class="color-svg">
+        <a href="mailto:m.bluth@example.com">margot.thiery@gmail.com</a>
+    </div>
+</div>
+
+<style>
+.color-svg {
+    filter: invert(54%) sepia(54%) saturate(6917%) hue-rotate(224deg) brightness(97%) contrast(94%);
+    margin-right: 10px;
+}
+.content {
+    display: flex;
+    justify-content: space-around;
+}
+.item {
+    display: flex;
+    align-items: center;
+}
+</style>
+
+</docs>
