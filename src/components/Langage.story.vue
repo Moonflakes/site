2ba220970b4d1a645714da534631047cd0b34d14
Moonflakes <33527879+Moonflakes@users.noreply.github.com>
2023-06-14 14:26:48 +0200

    add stories

---
+++ src/components/Langage.story.vue
@@ -0,0 +1,15 @@
+<template>
+  <Story
+    title="Langues"
+    group="top3"
+    icon="mdi:translate"
+    :layout="{ type: 'grid', width: '50%' }"
+  >
+    <Variant title="French">
+      <h2>Français</h2>
+    </Variant>
+    <Variant title="English">
+      <h2>Anglais</h2>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/Professional.story.vue
@@ -0,0 +1,15 @@
+<template>
+  <Story
+    title="Expérience pro"
+    group="top3"
+    icon="mdi:briefcase"
+    :layout="{ type: 'grid', width: '100%' }"
+  >
+    <Variant title="Exp 1">
+      <h2>Exp 1</h2>
+    </Variant>
+    <Variant title="Exp2">
+      <h2>Exp 2</h2>
+    </Variant>
+  </Story>
+</template>
